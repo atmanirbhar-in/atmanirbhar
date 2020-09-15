0205c4b65178a78529003b052682b057351c65a7
Sandesh Soni <hello@sandeshsoni.com>
2020-09-15 14:41:32 +0530

    move config to root

--- config/config.exs
+++ config/config.exs
@@ -1,18 +1,51 @@
-# This file is responsible for configuring your umbrella
-# and **all applications** and their dependencies with the
-# help of the Config module.
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
 #
-# Note that all applications in your umbrella share the
-# same configuration and dependencies, which is why they
-# all use the same configuration file. If you want different
-# configurations or dependencies per app, it is best to
-# move said applications out of the umbrella.
-import Config
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
 
-# Sample configuration:
-#
-#     config :logger, :console,
-#       level: :info,
-#       format: "$date $time [$level] $metadata$message\n",
-#       metadata: [:user_id]
-#
+# General application configuration
+use Mix.Config
+
+config :atmanirbhar,
+  ecto_repos: [Atmanirbhar.Repo]
+
+# Configures the endpoint
+config :atmanirbhar, AtmanirbharWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "fi3WS1GueDLkVqTKlT8GfqWTMaYBWdYf92IU/nHQExuZ/wif6OPk3KvDHL5ywKX5",
+  render_errors: [view: AtmanirbharWeb.ErrorView, accepts: ~w(html json), layout: false],
+  pubsub_server: Atmanirbhar.PubSub,
+  live_view: [signing_salt: "ZZfCCIhx"]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+config :waffle,
+  storage: Waffle.Storage.S3, # or Waffle.Storage.Local
+  bucket: System.get_env("AWS_BUCKET_NAME") # if using S3
+
+# If using S3:
+config :ex_aws,
+  json_codec: Jason,
+  access_key_id: System.get_env("AWS_ACCESS_KEY_ID"),
+  secret_access_key: System.get_env("AWS_SECRET_ACCESS_KEY"),
+  region: System.get_env("AWS_REGION")
+
+config :atmanirbhar, Atmanirbhar.Mailer,
+  adapter: Bamboo.MandrillAdapter,
+  api_key: "my_api_key"
+
+config :kaffy,
+   otp_app: :atmanirbhar,
+   ecto_repo: Atmanirbhar.Repo,
+   router: AtmanirbharWeb.Router
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
