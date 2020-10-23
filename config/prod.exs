c974b23b1385ab72569f8a58aa6fb1ae97069a53
Sandesh Soni <hello@sandeshsoni.com>
2020-10-23 23:00:44 +0530

    fix socket issues

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,8 @@ use Mix.Config
 # before starting your production server.
 config :atmanirbhar, AtmanirbharWeb.Endpoint,
 #  url: [host: "example.com", port: 80],
-  url: [host: "atmanirbhar.in", port: 80], 
+  url: [host: "atmanirbhar.in", port: 80],
+  check_origin: ["//localhost", "//atmanirbhar.in"],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
