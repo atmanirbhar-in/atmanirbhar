d6ff870d9cf58b9758211b359a4c3ee6a31a069a
Sandesh Soni <hello@sandeshsoni.com>
2020-11-28 01:19:06 +0530

    wip - FIX process for correct blocks

--- apps/atmanirbhar/lib/atmanirbhar/catalog/taxonomy_importer.ex
+++ apps/atmanirbhar/lib/atmanirbhar/catalog/taxonomy_importer.ex
@@ -3,111 +3,128 @@ defmodule Atmanirbhar.Catalog.TaxonomyImporter do
   @file_name "taxonomy-with-ids.en-US.csv"
 
   def say_hello do
+    # NimbleCSV.define(MyParser, separator: "\t", escape: "\"")
     file_path = Path.join("apps/atmanirbhar/priv/repo/", @file_name)
-    IO.puts "Hello, World!, #{file_path}"
-    # IO.puts File.exists?(file_path)
-    # IO.puts "-----"
-    # IO.puts inspect File.read(file_path)
+    NimbleCSV.define(MyParser, separator: "\t", escape: "\"")
+
+    file_path
+    |> File.stream!
+    |> NimbleCSV.RFC4180.parse_stream([skip_headers: false])
+    |> Stream.map(fn set_of_records ->
+      Enum.zip([:uniq, :cat0, :cat1, :cat2, :cat3, :cat4, :cat5, :cat6],
+        set_of_records
+      )
+      |> Enum.into(%{})
+      |> process
+    end)
+    |> Stream.run
+
   end
 
-  # just cat0
-  defp process(row) do
-    with {:ok, cat0} <- Map.fetch(row, "cat0"),
-         :error <- Map.fetch(row, "cat1"),
-           :error <- Map.fetch(row, "cat2"),
-           :error <- Map.fetch(row, "cat3"),
-           :error <- Map.fetch(row, "cat4"),
-           :error <- Map.fetch(row, "cat5"),
-           :error <- Map.fetch(row, "cat6")
-      do
-      #
-    end
+  # upto cat 1
+  defp process(%{
+        uniq: uniq,
+        cat0: cat0,
+        cat1: "",
+        cat2: "",
+        cat3: "",
+        cat4: "",
+        cat5: "",
+        cat6: ""
+               }) do
+    IO.puts "------"
+    IO.puts "just cat0"
+    IO.puts cat0
+    IO.puts "------"
   end
 
-  # just cat1
-  defp process(row) do
-    with {:ok, cat0} <- Map.fetch(row, "cat0"),
-         {:ok, cat1} <- Map.fetch(row, "cat1"),
-           :error <- Map.fetch(row, "cat2"),
-           :error <- Map.fetch(row, "cat3"),
-           :error <- Map.fetch(row, "cat4"),
-           :error <- Map.fetch(row, "cat5"),
-           :error <- Map.fetch(row, "cat6")
-      do
-      #
-    end
+  # upto cat 2
+  defp process(%{
+        uniq: uniq,
+        cat0: cat0,
+        cat1: cat1,
+        cat2: "",
+        cat3: "",
+        cat4: "",
+        cat5: "",
+        cat6: ""
+               }) do
+    IO.puts "------"
+    IO.puts "upto cat1"
+    IO.puts cat0
+    IO.puts cat1
+    IO.puts "------"
   end
 
-  # just cat2
-  defp process(row) do
-    with {:ok, cat0} <- Map.fetch(row, "cat0"),
-         {:ok, cat1} <- Map.fetch(row, "cat1"),
-         {:ok, cat2} <- Map.fetch(row, "cat2"),
-           :error <- Map.fetch(row, "cat3"),
-           :error <- Map.fetch(row, "cat4"),
-           :error <- Map.fetch(row, "cat5"),
-           :error <- Map.fetch(row, "cat6")
-      do
-      #
-    end
+  # upto cat 2
+  defp process(%{
+        uniq: uniq,
+        cat0: cat0,
+        cat1: cat1,
+        cat2: cat2,
+        cat3: "",
+        cat4: "",
+        cat5: "",
+        cat6: ""
+               }) do
+    IO.puts "upto cat2"
+  end
+
+  # upto cat 3
+  defp process(%{
+        uniq: uniq,
+        cat0: cat0,
+        cat1: cat1,
+        cat2: cat2,
+        cat3: cat3,
+        cat4: "",
+        cat5: "",
+        cat6: ""
+               }) do
+    IO.puts "upto cat3"
   end
 
 
-  # just cat3
-  defp process(row) do
-    with {:ok, cat0} <- Map.fetch(row, "cat0"),
-         {:ok, cat1} <- Map.fetch(row, "cat1"),
-         {:ok, cat2} <- Map.fetch(row, "cat2"),
-         {:ok, cat3} <- Map.fetch(row, "cat3"),
-           :error <- Map.fetch(row, "cat4"),
-           :error <- Map.fetch(row, "cat5"),
-           :error <- Map.fetch(row, "cat6")
-      do
-      #
-    end
+  # upto cat 4
+  defp process(%{
+        uniq: uniq,
+        cat0: cat0,
+        cat1: cat1,
+        cat2: cat2,
+        cat3: cat3,
+        cat4: cat4,
+        cat5: "",
+        cat6: ""
+               }) do
+    IO.puts "upto cat4"
   end
 
-  # just cat4
-  defp process(row) do
-    with {:ok, cat0} <- Map.fetch(row, "cat0"),
-         {:ok, cat1} <- Map.fetch(row, "cat1"),
-         {:ok, cat2} <- Map.fetch(row, "cat2"),
-         {:ok, cat3} <- Map.fetch(row, "cat3"),
-         {:ok, cat4} <- Map.fetch(row, "cat4"),
-           :error <- Map.fetch(row, "cat5"),
-           :error <- Map.fetch(row, "cat6")
-      do
-      #
-    end
+  # upto cat 5
+  defp process(%{
+        uniq: uniq,
+        cat0: cat0,
+        cat1: cat1,
+        cat2: cat2,
+        cat3: cat3,
+        cat4: cat4,
+        cat5: cat5,
+        cat6: ""
+               }) do
+    IO.puts "upto cat5"
   end
 
-  # just cat5
-  defp process(row) do
-    with {:ok, cat0} <- Map.fetch(row, "cat0"),
-         {:ok, cat1} <- Map.fetch(row, "cat1"),
-         {:ok, cat2} <- Map.fetch(row, "cat2"),
-         {:ok, cat3} <- Map.fetch(row, "cat3"),
-         {:ok, cat4} <- Map.fetch(row, "cat4"),
-         {:ok, cat5} <- Map.fetch(row, "cat5"),
-           :error <- Map.fetch(row, "cat6")
-      do
-      #
-    end
+  # upto cat6
+  defp process(%{
+        uniq: uniq,
+        cat0: cat0,
+        cat1: cat1,
+        cat2: cat2,
+        cat3: cat3,
+        cat4: cat4,
+        cat5: cat5,
+        cat6: cat6
+               }) do
+    IO.puts "upto cat6"
   end
-
-  # just cat6
-  defp process(row) do
-    with {:ok, cat0} <- Map.fetch(row, "cat0"),
-         {:ok, cat1} <- Map.fetch(row, "cat1"),
-         {:ok, cat2} <- Map.fetch(row, "cat2"),
-         {:ok, cat3} <- Map.fetch(row, "cat3"),
-         {:ok, cat4} <- Map.fetch(row, "cat4"),
-         {:ok, cat5} <- Map.fetch(row, "cat5"),
-         {:ok, cat6} <- Map.fetch(row, "cat6")
-      do
-      #
-    end
-  end
-
 
 end
