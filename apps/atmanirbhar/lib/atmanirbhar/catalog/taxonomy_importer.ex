f253e4e23a221d95bf9a43b5271bbde2552cd924
Sandesh Soni <hello@sandeshsoni.com>
2020-11-27 19:59:00 +0530

    wip - test dummy hello world message

--- apps/atmanirbhar/lib/atmanirbhar/catalog/taxonomy_importer.ex
+++ apps/atmanirbhar/lib/atmanirbhar/catalog/taxonomy_importer.ex
@@ -1,7 +1,11 @@
 defmodule Atmanirbhar.Catalog.TaxonomyImporter do
 
+  @file_name "taxonomy-with-ids.en-US.csv"
+
   def say_hello do
-    IO.puts "Hello, World!"
+    IO.puts "Hello, World!, #{@file_name}"
+    IO.puts File.exists?("/priv/repo/" <> @file_name)
+    IO.puts "-----"
   end
 
 end
