d1227622c87e407be28330197d8c5f25e7a7b781
Sandesh Soni <hello@sandeshsoni.com>
2020-12-18 13:33:19 +0530

    wip - stall display products

---
+++ apps/atmanirbhar/lib/atmanirbhar/marketplace/audience_age.ex
@@ -0,0 +1,35 @@
+defmodule Atmanirbhar.Marketplace.AudienceAge do
+
+  @moduledoc """
+
+  Audience as.
+  0, new born, about to born
+  1, playing
+  2. school going
+  3. university
+  4. Bachelor
+  5. Working, formal tie
+  6. Old
+  7. Couple
+  8. Family
+  9. Pets & Plants
+  10 Others
+
+  A Map of key value.
+  """
+
+  @doc """
+  for_male
+  for_female
+  for_both
+  """
+  def gender(opt) do
+  end
+
+  # discount vouchers
+  # collection variety.
+  def what_to_dos do
+  end
+
+
+end
--- apps/atmanirbhar/lib/atmanirbhar_web/live/page_stall_live.ex
+++ apps/atmanirbhar/lib/atmanirbhar_web/live/page_stall_live.ex
@@ -13,30 +13,25 @@ defmodule AtmanirbharWeb.PageStallLive do
     # IO.puts "host in socket? -----------------"
 
     # Atmanirbhar.Repo.put_org_id(123)
+    pincode = params["pincode"] || 12345
+    location_form = %LocationForm{pincode: pincode}
+    pincode_changeset = Marketplace.change_location_form(location_form)
 
+    pictures = [
+      "/toys/toys1.jpg",
+      "/toys/toys2.jpg",
+      "/toys/toys3.jpg",
+      "/toys/toys4.jpg",
+      "/toys/toys5.jpg",
+      "/toys/toys6.jpg",
+               ]
 
     socket = socket
     |> assign(:page_title, "View Stall")
-    # |> assign(location_form: location_form)
-    # |> assign(pincode_changeset: pincode_changeset)
-    # |> assign(reader_count: initial_count)
-    # |> assign(deals: deals)
-    # |> assign(advertisements: advertisements)
-    # |> assign(shops: shops)
+    |> assign(:pictures, pictures)
+    |> assign(pincode_changeset: pincode_changeset)
 
     {:ok, socket}
-
-    # assign(socket, %{
-    #       query: "",
-    #       results: %{},
-    #       location_form: location_form,
-    #       pincode_changeset: pincode_changeset,
-    #       shops: shops,
-    #       deals: deals,
-    #       reader_count: initial_count,
-    #       advertisements: advertisements
-    #        }
-    # )
 
   end
 
@@ -54,8 +49,16 @@ defmodule AtmanirbharWeb.PageStallLive do
   end
 
   defp apply_action(socket, :index, _params) do
+
+    pictures = [
+      "/toys/toys2.jpg",
+      "/toys/toys8.jpg",
+      "/toys/toys7.jpg",
+    ]
+
     socket
     |> assign(:page_title, "stall - ")
+    |> assign(:pictures, pictures)
   end
   defp apply_action(socket, :pincode, params) do
     pincode = params["pincode"] |> String.to_integer
