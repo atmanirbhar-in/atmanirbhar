16eb21cb26cc8951ada82daf1ba3c24b66281e3f
Sandesh Soni <hello@sandeshsoni.com>
2021-01-04 02:24:50 +0530

    create product form, skeleton

---
+++ apps/atmanirbhar/lib/atmanirbhar_web/live/user_dashboard_live/product_form_component.ex
@@ -0,0 +1,46 @@
+defmodule AtmanirbharWeb.UserDashboardLive.ProductFormComponent do
+  use AtmanirbharWeb, :live_component
+
+  alias Atmanirbhar.Marketplace.{StallElement, Stall}
+
+  @impl true
+  def handle_event("validate", %{"product" => product_params}, socket) do
+    changeset =
+      socket.assigns.product
+      |> Marketplace.change_product(product_params)
+      |> Map.put(:action, :validate)
+
+    {:noreply, assign(socket, :changeset, changeset)}
+  end
+
+  def handle_event("save", %{"product" => product_params}, socket) do
+    save_product(socket, socket.assigns.action, product_params)
+  end
+
+  defp save_product(socket, :edit, product_params) do
+    case Marketplace.update_product(socket.assigns.product, product_params) do
+      {:ok, _product} ->
+        {:noreply,
+         socket
+         |> put_flash(:info, "Product updated successfully")
+         |> push_redirect(to: socket.assigns.return_to)}
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        {:noreply, assign(socket, :changeset, changeset)}
+    end
+  end
+
+  defp save_product(socket, :new, product_params) do
+    case Marketplace.create_product(product_params) do
+      {:ok, _product} ->
+        {:noreply,
+         socket
+         |> put_flash(:info, "Product created successfully")
+         |> push_redirect(to: socket.assigns.return_to)}
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        {:noreply, assign(socket, changeset: changeset)}
+    end
+  end
+
+end
