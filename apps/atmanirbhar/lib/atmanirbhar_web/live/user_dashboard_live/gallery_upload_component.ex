cd0033552a427f0950d87a4018bfcf849c523ef3
Sandesh Soni <hello@sandeshsoni.com>
2021-01-16 02:06:23 +0530

    temp fix upload btn

--- apps/atmanirbhar/lib/atmanirbhar_web/live/user_dashboard_live/gallery_upload_component.ex
+++ apps/atmanirbhar/lib/atmanirbhar_web/live/user_dashboard_live/gallery_upload_component.ex
@@ -1,5 +1,12 @@
 defmodule AtmanirbharWeb.UserDashboardLive.GalleryUploadComponent do
   use AtmanirbharWeb, :live_component
+  alias Atmanirbhar.Marketplace
+
+  def mount(socket) do
+    {:ok,
+     allow_upload(socket, :picture, accept: [".png", ".jpg"], max_entries: 4)
+    }
+  end
 
   # socket, session, params?
   def update(%{gallery_upload: input_gallery_upload} = assigns, socket) do
@@ -9,5 +16,13 @@ defmodule AtmanirbharWeb.UserDashboardLive.GalleryUploadComponent do
      |> assign(:changeset, changeset)}
   end
 
+  def handle_event("validate", %{"gallery_upload" => gallery_upload_params}, socket) do
+    changeset =
+      socket.assigns.gallery_upload
+      |> Marketplace.change_gallery_upload(gallery_upload_params)
+      |> Map.put(:action, :validate)
+
+    {:noreply, assign(socket, :changeset, changeset)}
+  end
 
 end
