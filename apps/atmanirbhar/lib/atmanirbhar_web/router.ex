0a98d4f833421bb6f693fe7a0eda1fe6e9b7e20e
Sandesh Soni <hello@sandeshsoni.com>
2021-01-18 21:45:46 +0530

    stall edit - show edit modal

--- apps/atmanirbhar/lib/atmanirbhar_web/router.ex
+++ apps/atmanirbhar/lib/atmanirbhar_web/router.ex
@@ -172,7 +172,7 @@ defmodule AtmanirbharWeb.Router do
     live "/add-my-business", UserDashboardLive.Index, :new_business
     live "/edit-business/:business_id", UserDashboardLive.Index, :edit_business
     live "/new_stall", UserDashboardLive.Index, :new_stall
-    live "/edit_stall/:stall_id", UserDashboardStallLive.Index, :edit_stall
+    live "/edit_stall/:stall_id", UserDashboardLive.Index, :edit_stall
     live "/dashboard/gallery", UserDashboardLive.Gallery, :gallery
     live "/dashboard/new_picture", UserDashboardLive.Gallery, :new_picture
     live "/bulk-upload", UserDashboardLive.Index, :new_bulk_upload
