0cfdcdb2e1a0f076e9877a7d55292ab5204de96b
Sandesh Soni <hello@sandeshsoni.com>
2021-01-26 19:47:20 +0530

    wip - upload pic

--- apps/atmanirbhar/lib/atmanirbhar_web/live/user_dashboard_live/gallery_upload_component.ex
+++ apps/atmanirbhar/lib/atmanirbhar_web/live/user_dashboard_live/gallery_upload_component.ex
@@ -35,26 +35,38 @@ defmodule AtmanirbharWeb.UserDashboardLive.GalleryUploadComponent do
   end
 
   def handle_event("save",
-    %{"gallery_upload" => gallery_params} = params,
-    socket) do
-    IO.puts inspect(socket.assigns.user_token)
-    IO.puts "-=-=-=-=-=-=-=-=-=-="
-    # save creates new records
-    # and stores to DB
-    # IO.puts "save gallery_upload"
+    %{"gallery_upload" => gallery_params} = params, socket) do
+    list_of_pictures = put_image_urls(socket)
+    business = Marketplace.get_business!(socket.assigns.business_id)
+    # Marketplace.create_media(socket.business_id, list_of_pictures)
+    case Marketplace.create_media(business, gallery_params, &consume_pictures(socket, &1)) do
+      {:ok, _bulk_upload} ->
+        {:noreply,
+         socket
+         |> put_flash(:info, "Photos uploaded to Gallery successfully")
+         |> push_redirect(to: socket.assigns.return_to)}
 
-    {:noreply, socket}
+      {:error, %Ecto.Changeset{} = changeset} ->
+        {:noreply, assign(socket, changeset: changeset)}
+    end
   end
 
+  defp put_image_urls(socket) do
+    {completed, [] } = uploaded_entries(socket, :picture)
+    image_urls = for entry <- completed do
+      Routes.static_path(socket, "/uploads/#{entry.uuid}.#{ext(entry)}")
+    end
+  end
 
+  def consume_pictures(socket, business) do
+    consume_uploaded_entries(socket, :picture, fn meta, entry ->
+      dest = Path.join("priv/static/uploads", "#{entry.uuid}.#{ext(entry)}")
+      File.cp!(meta.path, dest)
+    end)
+    {:ok, business}
+  end
 
+  defp ext(entry) do
+    [ext | _] = MIME.extensions(entry.client_type)
+  end
 end
-
-
-
-
-
-
-
-
-
