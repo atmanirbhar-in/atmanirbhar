cc79eb36aeef20037e51dea2611533d4eda1ced8
Sandesh Soni <hello@sandeshsoni.com>
2021-01-29 12:12:26 +0530

    make popup modal full-width, top of page on dashIndex

--- apps/atmanirbhar/lib/atmanirbhar_web/live/modal_component.ex
+++ apps/atmanirbhar/lib/atmanirbhar_web/live/modal_component.ex
@@ -22,7 +22,7 @@ defmodule AtmanirbharWeb.ModalComponent do
     <div class="absolute inset-0 bg-gray-800 opacity-75"></div>
     </div>
 
-    <div x-show="open" x-transition:enter="ease-out duration-300" x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100" x-transition:leave="ease-in duration-200" x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100" x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" class="bg-white rounded-lg overflow-hidden shadow-xl transform transition-all sm:max-w-lg sm:w-full">
+    <div x-show="open" x-transition:enter="ease-out duration-300" x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100" x-transition:leave="ease-in duration-200" x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100" x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" class="bg-white rounded-lg overflow-hidden shadow-xl transform transition-all sm:w-full">
     <div @click.away="open = false" class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
     <div class="hidden sm:block absolute top-0 right-0 pt-4 pr-4">
 
