ac7a17dedc21664b26f045939f3a74ddf15d723c
Sandesh Soni <hello@sandeshsoni.com>
2021-02-01 18:04:11 +0530

    reduce some code, remove old files

--- apps/atmanirbhar/lib/atmanirbhar/catalog/events.ex
+++ apps/atmanirbhar/lib/atmanirbhar/catalog/events.ex
@@ -1,21 +1,21 @@
-defmodule Atmanirbhar.Catalog.Events do
+# defmodule Atmanirbhar.Catalog.Events do
 
-  def list do
-    ~w(
-      Diwali
-      Dussera
-      New Year
-      Christmas
-      Friendship Day
-      Makar Sankranti
-    )
-  end
+#   def list do
+#     ~w(
+#       Diwali
+#       Dussera
+#       New Year
+#       Christmas
+#       Friendship Day
+#       Makar Sankranti
+#     )
+#   end
 
-  def yearly do
-    ~w(
-      birthday
-      anniversary
-    )
-  end
+#   def yearly do
+#     ~w(
+#       birthday
+#       anniversary
+#     )
+#   end
 
-end
+# end
--- apps/atmanirbhar/lib/atmanirbhar/marketplace/gallery_item.ex
+++ apps/atmanirbhar/lib/atmanirbhar/marketplace/gallery_item.ex
@@ -1,54 +1,54 @@
-defmodule Atmanirbhar.Marketplace.GalleryItem do
-  use Ecto.Schema
-  import Ecto.Changeset
-  alias Atmanirbhar.Accounts.User
-  alias Atmanirbhar.Marketplace.{Stall, StallItem, UserGallery}
+# defmodule Atmanirbhar.Marketplace.GalleryItem do
+#   use Ecto.Schema
+#   import Ecto.Changeset
+#   alias Atmanirbhar.Accounts.User
+#   alias Atmanirbhar.Marketplace.{Stall, StallItem, UserGallery}
 
-  schema "gallery_items" do
-    field :description, :string
-    field :images, {:array, :string}, nil: false, default: []
-    field :title, :string
-    field :type, :integer
+#   schema "gallery_items" do
+#     field :description, :string
+#     field :images, {:array, :string}, nil: false, default: []
+#     field :title, :string
+#     field :type, :integer
 
-    timestamps()
-    many_to_many(:stalls, Stall, join_through: StallItem, on_replace: :delete)
-    belongs_to :gallery, UserGallery
-  end
+#     timestamps()
+#     many_to_many(:stalls, Stall, join_through: StallItem, on_replace: :delete)
+#     belongs_to :gallery, UserGallery
+#   end
 
-  @doc """
-  1 - Product
-  2 - Timeline-item
-  3 - Deal
-  4 - promoted Ad
-  1 - Product
-  2 - Timeline-item
-  3 - Deal
-  4 - promoted Ad
-  """
-  def changeset(stall_element, attrs) do
-    stall_element
-    |> cast(attrs, [:type, :images, :description, :title])
-    # |> maybe_mark_for_deletion
-  end
+#   @doc """
+#   1 - Product
+#   2 - Timeline-item
+#   3 - Deal
+#   4 - promoted Ad
+#   1 - Product
+#   2 - Timeline-item
+#   3 - Deal
+#   4 - promoted Ad
+#   """
+#   def changeset(stall_element, attrs) do
+#     stall_element
+#     |> cast(attrs, [:type, :images, :description, :title])
+#     # |> maybe_mark_for_deletion
+#   end
 
-  def product_changeset(stall_element, attrs) do
-    stall_element
-    |> cast(attrs, [:title, :description, :images])
-    |> validate_required([:title, :description])
-  end
+#   def product_changeset(stall_element, attrs) do
+#     stall_element
+#     |> cast(attrs, [:title, :description, :images])
+#     |> validate_required([:title, :description])
+#   end
 
-  def timeline_post_changeset(stall_element, attrs) do
-    stall_element
-    |> cast(attrs, [:title, :description, :images])
-    |> validate_required([:title, :description])
-  end
+#   def timeline_post_changeset(stall_element, attrs) do
+#     stall_element
+#     |> cast(attrs, [:title, :description, :images])
+#     |> validate_required([:title, :description])
+#   end
 
-  # defp maybe_mark_for_deletion(changeset) do
-  #   if get_change(changeset, :delete) do
-  #     %{changeset | action: :delete}
-  #   else
-  #     changeset
-  #   end
-  # end
+#   # defp maybe_mark_for_deletion(changeset) do
+#   #   if get_change(changeset, :delete) do
+#   #     %{changeset | action: :delete}
+#   #   else
+#   #     changeset
+#   #   end
+#   # end
 
-end
+# end
